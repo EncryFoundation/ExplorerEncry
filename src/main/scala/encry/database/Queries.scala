2e1743050f5c95c0e4e4b9f5a433ce074b2ff229
YERMLV <germolaev@innodata.ru>
2019-08-13 13:34:00 +0300

    Reduced amount of logs in custom logger

--- src/main/scala/encry/database/Queries.scala
+++ src/main/scala/encry/database/Queries.scala
@@ -19,31 +19,28 @@ object Queries extends StrictLogging {
   implicit val jdkLogHandler: LogHandler = {
     LogHandler {
 
-      case Success(s, a, e1, e2) =>
+      case Success(s, _, e1, e2) =>
         logger.info(s"""Successful Statement Execution:
                           |
                           |  ${s.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
                           |
-                          | arguments = [${a.mkString(", ")}]
                           |   elapsed = ${e1.toMillis} ms exec + ${e2.toMillis} ms processing (${(e1 + e2).toMillis} ms total)
       """.stripMargin)
 
-      case ProcessingFailure(s, a, e1, e2, t) =>
+      case ProcessingFailure(s, _, e1, e2, t) =>
         logger.info(s"""Failed Resultset Processing:
                             |
                             |  ${s.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
                             |
-                            | arguments = [${a.mkString(", ")}]
                             |   elapsed = ${e1.toMillis} ms exec + ${e2.toMillis} ms processing (failed) (${(e1 + e2).toMillis} ms total)
                             |   failure = ${t.getMessage}
       """.stripMargin)
 
-      case ExecFailure(s, a, e1, t) =>
+      case ExecFailure(s, _, e1, t) =>
         logger.info(s"""Failed Statement Execution:
                             |
                             |  ${s.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
                             |
-                            | arguments = [${a.mkString(", ")}]
                             |   elapsed = ${e1.toMillis} ms exec (failed)
                             |   failure = ${t.getMessage}
       """.stripMargin)
