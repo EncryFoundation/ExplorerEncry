53ea1573779c80aaad5a2864ee745b81435a981e
capdev <sergey.moskalenko@scruge.world>
2019-10-16 11:27:20 +0500

    modifiers request modifiers receive and deserialize

--- src/main/scala/encry/net/network/NetworkMessagesHandler.scala
+++ src/main/scala/encry/net/network/NetworkMessagesHandler.scala
@@ -1,14 +1,17 @@
 package encry.net.network
 
+import HeaderProto.HeaderProtoMessage
+import PayloadProto.PayloadProtoMessage
 import TransactionProto.TransactionProtoMessage
 import akka.actor.{Actor, Props}
 import com.typesafe.scalalogging.StrictLogging
 import org.encryfoundation.common.utils.Algos
 import encry.net.network.BasicMessagesRepo._
-import encry.net.network.NetworkMessagesHandler.{BroadcastInvForTx, TransactionForCommit}
+import encry.net.network.NetworkMessagesHandler.{BroadcastInvForTx, ReceiveHeader, ReceivePayload, ReceiveTransaction, TransactionForCommit}
 import encry.net.modifiers.{Transaction, TransactionProtoSerializer}
 import encry.net.utils.CoreTaggedTypes.{ModifierId, ModifierTypeId}
 import encry.net.utils.CoreTaggedTypes
+import org.encryfoundation.common.modifiers.history.{Header, HeaderProtoSerializer, Payload, PayloadProtoSerializer}
 import supertagged.@@
 
 //TODO: replace everywhere encry.net to org.encryfoundation.common
@@ -24,23 +27,33 @@ class NetworkMessagesHandler() extends Actor with StrictLogging {
 
     case MessageFromNetwork(message, peerOpt) => message match {
 
-      case InvNetworkMessage(invData) if invData._1 == Transaction.modifierTypeId =>
-        logger.debug(s"Got transactionIds: ${invData._2.map(Algos.encode).mkString(",")}")
-        peerOpt.foreach { peer =>
-          logger.debug(s"Request transactions: ${invData._2.map(Algos.encode).mkString(",")}")
-          peer.handlerRef ! RequestModifiersNetworkMessage((invData._1, invData._2))
-        }
-
-      case ModifiersNetworkMessage((modifierTypeId, modifiers)) =>
-        logger.debug(s"Response modifiers: $modifierTypeId size ${modifiers.size}")
-        val transactions = modifiers.flatMap {case (modifierId, bytes) =>
-          TransactionProtoSerializer.fromProto(TransactionProtoMessage.parseFrom(bytes)).toOption
-        }
-        logger.debug(s"Extracted transactions: $transactions")
-
       case InvNetworkMessage((modifierTypeId, modifierIds)) =>
-        logger.debug(s"Inv message : $modifierTypeId ${modifierIds.map(Algos.encode).mkString(",")}")
+        logger.debug(s"Got modifiers: $modifierTypeId (${modifierIds.map(Algos.encode).mkString(",")})")
+        peerOpt.foreach { peer =>
+          if (List(Transaction.modifierTypeId, Header.modifierTypeId, Payload.modifierTypeId).contains(modifierTypeId)) {
+            logger.debug(s"Request modifier: $modifierTypeId ${modifierIds.map(Algos.encode).mkString(",")}")
+            peer.handlerRef ! RequestModifiersNetworkMessage((modifierTypeId, modifierIds))
+          }
+        }
 
+      case ModifiersNetworkMessage((modifierTypeId, modifierMap)) =>
+        logger.debug(s"Response modifiers: $modifierTypeId size ${modifierMap.size}")
+
+        modifierMap.foreach { case (modifierId, bytes) =>
+          modifierTypeId match {
+            case Transaction.modifierTypeId =>
+              val tx = TransactionProtoSerializer.fromProto(TransactionProtoMessage.parseFrom(bytes))
+              tx.foreach(target ! ReceiveTransaction(_))
+
+            case Header.modifierTypeId =>
+              val header = HeaderProtoSerializer.fromProto(HeaderProtoMessage.parseFrom(bytes))
+              header.foreach(target ! ReceiveHeader(_))
+
+            case Payload.modifierTypeId =>
+              val payload = PayloadProtoSerializer.fromProto(PayloadProtoMessage.parseFrom(bytes))
+              payload.foreach(target ! ReceivePayload(_))
+          }
+        }
 
       case RequestModifiersNetworkMessage(invData) if invData._1 == Transaction.modifierTypeId =>
         logger.debug(s"Got request modifiers on NMH")
@@ -62,7 +75,15 @@ class NetworkMessagesHandler() extends Actor with StrictLogging {
 }
 
 object NetworkMessagesHandler {
+
+  case class ReceiveTransaction(tx: Transaction)
+
+  case class ReceiveHeader(header: Header)
+
+  case class ReceivePayload(payload: Payload)
+
   case class TransactionForCommit(tx: Transaction)
+
   case class BroadcastInvForTx(tx: Transaction)
 
   def props() = Props(new NetworkMessagesHandler())
